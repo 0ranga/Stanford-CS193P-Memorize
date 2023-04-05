ff9ca41d563d5db7fc5e258ec1d098323f5b622d
0ranga <jordan_louis@outlook.com>
2023-04-05 02:32:10 +0200

    Initial Commit

---
+++ Stanford-CS193P-Memorize/Stanford_CS193P_MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Stanford_CS193P_MemorizeApp.swift
+//  Stanford-CS193P-Memorize
+//
+//  Created by Heesham on 4/5/23.
+//
+
+import SwiftUI
+
+@main
+struct Stanford_CS193P_MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
