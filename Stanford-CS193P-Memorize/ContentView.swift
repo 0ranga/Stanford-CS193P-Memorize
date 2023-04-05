a3c6eb62379969ad5e2d69f370e67e2103407678
0ranga <jordan_louis@outlook.com>
2023-04-05 12:30:30 +0200

    created hello world

--- Stanford-CS193P-Memorize/ContentView.swift
+++ Stanford-CS193P-Memorize/ContentView.swift
@@ -8,16 +8,43 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
+    var body: some View { // this is a function
+        ZStack {
+            RoundedRectangle(cornerRadius: 25.0) // argument with a label "cornerRadius"
+                .stroke(lineWidth: 3)
+            
             Text("Hello, world!")
+                .foregroundColor(.orange)
         }
-        .padding()
+        .padding(.horizontal)
+        .foregroundColor(.red)
+        
+
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+//        return Text("Hello, world!") // what the function returns, Text also behaves like a view
+//            .foregroundColor(Color.orange)
+//            .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/) // .padding is a func that exists in all structs that behave like a view
+//        // padding func returns something that behaves like a view (a padded modified other view) ≠ than Text
+//        // padding is considered as a view modifier, when we use a view modifier it is going to return a new view
+        
     }
 }
+
+
+
+
+
+
+
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
