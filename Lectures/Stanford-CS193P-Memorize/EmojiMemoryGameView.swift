5c8be728da6c88a09217348ca7b6eb74ecad30e0
0ranga <jordan_louis@outlook.com>
2023-05-14 12:11:20 +0200

    started lecture 5 cleaned up some code, renamed variables

--- Lectures/Stanford-CS193P-Memorize/EmojiMemoryGameView.swift
+++ Lectures/Stanford-CS193P-Memorize/EmojiMemoryGameView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  EmojiMemoryGameView.swift
 //  Stanford-CS193P-Memorize
 //
 //  Created by Heesham on 4/5/23.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct EmojiMemoryGameView: View {
     @ObservedObject var viewModel : EmojiMemoryGame // @ObservedObject means that when this thing changes, we need to redraw the whole body
     
     var body: some View { // this is a function
@@ -53,13 +53,13 @@ struct CardView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         let game = EmojiMemoryGame()
-        ContentView(viewModel: game)
+        EmojiMemoryGameView(viewModel: game)
             .preferredColorScheme(.dark)
-        ContentView(viewModel: game)
+        EmojiMemoryGameView(viewModel: game)
             .preferredColorScheme(.light)
             .previewDevice("iPhone 12 mini")
             .previewDisplayName("iPhone 12")
-        ContentView(viewModel: game)
+        EmojiMemoryGameView(viewModel: game)
             .previewInterfaceOrientation(.landscapeLeft)
             .previewDisplayName("landscape")
             .preferredColorScheme(.light)
