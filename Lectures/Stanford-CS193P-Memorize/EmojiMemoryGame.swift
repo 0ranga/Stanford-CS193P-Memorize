9e58d24ece0774327f572ca33b3e587873abe8a9
0ranga <jordan_louis@outlook.com>
2023-05-15 23:20:03 +0200

    lesson 6 - feat: add pie shape behind emojis to animate later

--- Lectures/Stanford-CS193P-Memorize/EmojiMemoryGame.swift
+++ Lectures/Stanford-CS193P-Memorize/EmojiMemoryGame.swift
@@ -13,7 +13,7 @@ class EmojiMemoryGame: ObservableObject {
     private static let emojis: Array<String> = ["🚲", "🚂", "🚁", "🚜", "🚕", "🏎️", "🚑", "🚓", "🚒", "✈️", "🚀", "⛵️", "🛸", "🛶", "🚌", "🏍️", "🛺", "🚠", "🛵", "🚗", "🚚", "🚇", "🛻", "🚝"]
     
     private static func createMemoryGame() -> MemoryGame<String> {
-        MemoryGame<String>(numberOfPairsOfCards: 8) { pairIndex in
+        MemoryGame<String>(numberOfPairsOfCards: 2) { pairIndex in
             emojis[pairIndex]
         }
     }
