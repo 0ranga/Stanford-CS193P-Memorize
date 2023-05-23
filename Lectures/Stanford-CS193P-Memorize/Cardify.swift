1c2a2689bd365358e5adfe933096bc2101900e4d
0ranga <jordan_louis@outlook.com>
2023-05-23 23:52:31 +0200

    lecture 7 - feat: add cardify modifier

---
+++ Lectures/Stanford-CS193P-Memorize/Cardify.swift
@@ -0,0 +1,30 @@
+//
+//  Cardify.swift
+//  Stanford-CS193P-Memorize
+//
+//  Created by Jordan Louis on 5/23/23.
+//
+
+import SwiftUI
+
+struct Cardify: ViewModifier {
+    var isFaceUp: Bool
+    
+    func body(content: Content) -> some View {
+        ZStack {
+            let shape = RoundedRectangle(cornerRadius: DrawingConstants.cornerRadius)
+            if isFaceUp {
+                shape.fill().foregroundColor(.white)
+                shape.strokeBorder(lineWidth: DrawingConstants.lineWidth)
+                content
+            } else {
+                shape.fill()
+            }
+        }
+    }
+    
+    private struct DrawingConstants {
+        static let cornerRadius: CGFloat = 10
+        static let lineWidth: CGFloat = 3
+    }
+}
