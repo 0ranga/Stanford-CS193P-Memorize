bfa056d7b06e6b7100815dd54c5cd232586ac0bf
0ranga <jordan_louis@outlook.com>
2023-06-04 15:21:10 +0200

    lecture 9 - create EmojiArt project

---
+++ Lectures/EmojiArt/EmojiArt/EmojiArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiArtApp.swift
+//  EmojiArt
+//
+//  Created by Jordan Louis on 6/4/23.
+//
+
+import SwiftUI
+
+@main
+struct EmojiArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
